454fe5f94e91699e9efabca5c6427210b5a10387
Georgi <georgi_yosifov99@abv.bg>
2020-11-16 11:32:36 +0200

    First commit

---
+++ js/code.js
@@ -0,0 +1,84 @@
+$( document ).ready(function() {
+    let isPopUp = false;
+
+    // Function to change the nav-bar on scroll
+    $(window).scroll(function(){
+        ($(window).scrollTop() >= 100) ? (
+            $('.fixed-nav-bar').addClass('scrolled'),
+            $('.the-bass').addClass('scrolled'),
+            $('.fixed-nav-bar img').addClass('scrolled'),
+            $('.fixed-nav-bar .logo').addClass('scrolled')
+        ) : (
+            $('.fixed-nav-bar').removeClass('scrolled'),
+            $('.the-bass').removeClass('scrolled'),
+            $('.fixed-nav-bar img').removeClass('scrolled'),
+            $('.fixed-nav-bar .logo').removeClass('scrolled')
+        );
+    });
+    
+    // Drop Down Function
+    $('#menuButton').on('change', function(){
+        ($('#menuButton').is(':checked')) ? (
+            $('.the-bass').addClass('dropped')
+        ) : (
+            $('.the-bass').removeClass('dropped')
+        );
+    });
+
+    // Hover image  
+    $(".image-part, .image-part *").hover(
+        function() {
+            if (!isPopUp) {
+                $(this).find('.fa-expand-arrows-alt').addClass('expandBtn').removeClass('noneExpandBtn');
+            }
+        }, 
+        function() {
+            $(this).find('.fa-expand-arrows-alt').addClass('noneExpandBtn').removeClass('expandBtn');
+        }
+    );
+
+    // Pop up image
+    $('.image-main-img').click(function(e){   
+        if (!isPopUp) {
+            e.stopPropagation();
+            let currParent = $(e.target).parent();
+            currParent.addClass('popUp').removeClass('image-main');
+            $(e.target).removeClass("image-main-img");
+            currParent.children('.fa-expand-arrows-alt').addClass('noneExpandBtn').removeClass('expandBtn');
+            currParent.children('i').addClass('closeBtn').removeClass('noneCloseBtn'); 
+            isPopUp = true;
+        }
+    }); 
+    //Expand image
+    $('.fa-expand-arrows-alt').click(function(e){
+        if (!isPopUp) {
+            e.stopPropagation();
+            let currParent = $(e.target).parent();
+            currParent.addClass('popUp').removeClass('image-main');
+            currParent.children('img').removeClass("image-main-img");
+            currParent.children('.fa-expand-arrows-alt').addClass('noneExpandBtn').removeClass('expandBtn');
+            currParent.children('.fa-times').addClass('closeBtn').removeClass('noneCloseBtn'); 
+            isPopUp = true;
+        } 
+    });
+
+    //Close pop up image
+    $("body").click(function(e){ 
+        if (isPopUp && !$(e.target).is('.popUp, .popUp *')) {
+            $('.popUp').children('img').addClass("image-main-img");
+            $('.popUp').children('.fa-times').addClass('noneCloseBtn').removeClass('closeBtn'); 
+            $('.popUp').children('.fa-expand-arrows-alt').addClass('expandBtn').removeClass('noneExpandBtn');
+            $('.popUp').addClass('image-main').removeClass('popUp');
+            isPopUp = false;
+        } 
+       
+    });
+    //Close pop up image
+    $('.fa-times').click(function(e){
+        $(e.target).parent().addClass('image-main').removeClass('popUp');
+        $(e.target).parent().children('img').addClass("image-main-img");
+        $(e.target).addClass('noneCloseBtn').removeClass('closeBtn');
+        $(e.target).parent().children('.fa-expand-arrows-alt').addClass('expandBtn').removeClass('noneExpandBtn'); 
+        isPopUp = false;  
+    });
+});
---
+++ js/template-data.js
@@ -0,0 +1,576 @@
+let sifoni114 = {
+    sifoni114: [
+        {
+            image:"sifoni/DSC_0079_.png",
+            name:"1-10",
+            description:"Сифон без преливник за мивка 1¼\""
+        },
+        {
+            image:"sifoni/DSC_0123_.png",
+            name:"1-24",
+            description:"Сифон без преливник за мивка 1¼\""
+        },
+        {
+            image:"sifoni/DSC_0143_.png",
+            name:"1-29",
+            description:"Сифон без преливник за мивка с гофрирана изходяща тръба 1¼\" /голям/"
+        },
+        {
+            image:"sifoni/DSC_0146_.png",
+            name:"1-31",
+            description:"Сифон без преливник за мивка с гофрирана изходяща тръба 1¼\" /малък/"
+        },
+        {
+            image:"sifoni/DSC_0188_.png",
+            name:"1-50",
+            description:"Сифон за стояща батерия 1¼\""
+        },
+        {
+            image:"sifoni/DSC_0190_.png",
+            name:"1-51",
+            description:"Сифон за писоар 1¼\""
+        },
+        {
+            image:"sifoni/DSC_0261_.png",
+            name:"1-10B1",
+            description:"Сифон без преливник за мивка 1¼\" /голям без изходяща тръба ф32 мм./"
+        },
+        {
+            image:"sifoni/IMG_20191212_122428.png",
+            name:"1-24B1",
+            description:"Сифон без преливник за мивка 1¼\" /малък без изходяща тръба ф32 мм./"
+        },
+        {
+            image:"sifoni/DSC_0274_.png",
+            name:"1-29B1",
+            description:"Сифон без преливник за мивка с гофрирана изходяща тръба с L=600 мм. 1¼\" /голям/"
+        },
+        {
+            image:"sifoni/DSC_0276_.png",
+            name:"1-31B1",
+            description:"Сифон без преливник за мивка с гофрирана изходяща тръба L=600 мм. 1¼\" /малък/"
+        }
+    ]
+};
+let sifoni112 = {
+    sifoni112: [
+        {
+            image:"sifoni/DSC_0082_.png",
+            name:"1-11",
+            description:"Сифон без преливник за мивка 1½\""
+        },
+        {
+            image:"sifoni/DSC_0085_.png",
+            name:"1-12",
+            description:"\"S\" сифон без преливник за мивка 1½\""
+        },
+        {
+            image:"sifoni/DSC_0091_.png",
+            name:"1-13",
+            description:"Сифон с преливник за мивка 1½\""
+        },
+        {
+            image:"sifoni/DSC_0093_.png",
+            name:"1-14",
+            description:"Сифон с преливник за вана 1½\""
+        },
+        {
+            image:"sifoni/DSC_0099_.png",
+            name:"1-16",
+            description:"\"S\" сифон за мивка без преливник и с извод за пералня 1½\""
+        },
+        {
+            image:"sifoni/DSC_0105_.png",
+            name:"1-17",
+            description:"\"S\" сифон с преливник за мивка и с извод за пералня 1½\""
+        },
+        {
+            image:"sifoni/DSC_0141_.png",
+            name:"1-30",
+            description:"Сифон за мивка с преливник и с гофрирана изходяща тръба 1½\""
+        },
+        {
+            image:"sifoni/DSC_0157_.png",
+            name:"1-35",
+            description:"Сифон за мивка без преливник и с извод за пералня ф90 мм."
+        },
+        {
+            image:"sifoni/DSC_0161_.png",
+            name:"1-36",
+            description:"Сифон за мивка с преливник ф90 мм."
+        },
+        {
+            image:"sifoni/DSC_0185_.png",
+            name:"1-48",
+            description:"Сифон за мивка  без преливник с гофрирана изходяща тръба 1½\""
+        },
+        {
+            image:"sifoni/DSC_0209_.png",
+            name:"1-57",
+            description:"\"S\" сифон за мивка без преливник и с изводи за пералня и миялна машина 1½\""
+        },
+        {
+            image:"sifoni/DSC_0211_.png",
+            name:"1-58",
+            description:"\"S\" сифон за мивка с преливник и с изводи за пералня и миялна машина 1½\""
+        },
+        {
+            image:"sifoni/DSC_0245_.png",
+            name:"1-73",
+            description:"Сифон без преливник за мивка с гофрирана изходяща тръба и с извод за пералня 1½\""
+        },
+        {
+            image:"sifoni/DSC_0262_.png",
+            name:"1-11B1",
+            description:"Сифон без преливник за мивка с нипел 1½\" /без изходяща тръба ф40 мм./"
+        },
+        {
+            image:"sifoni/DSC_0265_.png",
+            name:"1-13B1",
+            description:"Сифон с преливник и нипел за мивка 1½\" /без изходяща тръба ф40 мм./"
+        }
+    ]
+}
+let dvoiniSifoni112 = {
+    dvoiniSifoni112: [
+        {
+            image:"sifoni/DSC_0095_.png",
+            name:"1-15",
+            description:"\"S\" сифон с преливници и с извод за пералня за двукоритна мивка 1½\""
+        }, 
+        {
+            image:"sifoni/DSC_0110_.png",
+            name:"1-19",
+            description:"\"S\" сифон без преливници и с извод за пералня за двукоритна мивка 1½\""
+        },
+        {
+            image:"sifoni/DSC_0127_.png",
+            name:"1-25",
+            description:"Сифон с преливници за двукоритна мивка 1½\""
+        },
+        {
+            image:"sifoni/DSC_0129_.png",
+            name:"1-26",
+            description:"Сифон с преливници за двукоритна мивка 1½\""
+        },
+        {
+            image:"sifoni/DSC_0134_.png",
+            name:"1-27",
+            description:"Сифон без преливници за двукоритна мивка 1½\""
+        },
+        {
+            image:"sifoni/DSC_0132_.png",
+            name:"1-28",
+            description:"Сифон без преливници за двукоритна мивка 1½\""
+        },
+        {
+            image:"sifoni/DSC_0155_.png",
+            name:"1-34",
+            description:"\"S\" сифон с един преливник за двукоритна мивка 1½\""
+        },
+        {
+            image:"sifoni/DSC_0196_.png",
+            name:"1-53",
+            description:"\"S\" сифон с един преливник за двукоритна мивка ф90 мм."
+        },
+        {
+            image:"sifoni/DSC_0203_.png",
+            name:"1-55",
+            description:"\"S\" сифон за двукоритна мивка без преливници ф90 мм."
+        },
+        {
+            image:"sifoni/DSC_0206_.png",
+            name:"1-56",
+            description:"\"S\" сифон  за двукоритна мивка с преливници ф90 мм."
+        },
+        {
+            image:"sifoni/DSC_0290_.png",
+            name:"1-25B1",
+            description:"Сифон с преливници и с гофрирана тръба L=290 мм. за двукоритна мивка 1½\""
+        },
+        {
+            image:"sifoni/DSC_0292_.png",
+            name:"1-27B1",
+            description:"Сифон без преливници с гофрирана тръба L=290 мм. за двукоритна мивка 1½\""
+        }
+    ]
+}
+let podoviSifoni = {
+    podoviSifoni: [
+        {
+            image:"sifoni/DSC_0112_.png",
+            name:"1-20",
+            description:"Рогов подов сифон ф50/120х120х73 мм."
+        },
+        {
+            image:"sifoni/DSC_0116_.png",
+            name:"1-21",
+            description:"Прав подов сифон ф50 мм."
+        },
+        {
+            image:"sifoni/DSC_0180_.png",
+            name:"1-45",
+            description:"Рогов подов сифон ф50/110х110х73 мм./метална неръждаема решетка/"
+        },
+        {
+            image:"sifoni/DSC_0106_.png",
+            name:"1-18",
+            description:"Сифон за поддушово корито 1½\""
+        },
+        {
+            image:"sifoni/DSC_0149_.png",
+            name:"1-32",
+            description:"Сифон за поддушово корито 1½\" /малък/"
+        },
+        {
+            image:"sifoni/DSC_0151_.png",
+            name:"1-33",
+            description:"Сифон за порцеланово поддушово корито 1½\""
+        },
+        {
+            image:"sifoni/DSC_0278_.png",
+            name:"1-32B1",
+            description:"Сифон за поддушово корито 1½\" /малък/"
+        },
+        {
+            image:"sifoni/DSC_0282_.png",
+            name:"1-32B2",
+            description:"Сифон за поддушово корито с гофрирана изходяща тръба ф40 мм./L=600 мм. /малък/"
+        }
+    ]
+}
+let gofriraniTrubi = {
+    gofriraniTrubi: [
+        {
+            image:"sifoni/DSC_0214_.png",
+            name:"1-59",
+            description:"Сифон за мивка гофриран без преливник и с извод за пералня ф90 мм."
+        },
+        {
+            image:"sifoni/DSC_0215_.png",
+            name:"1-60",
+            description:"Сифон за мивка гофриран с преливник ф90 мм."
+        },
+        {
+            image:"sifoni/DSC_0217_.png",
+            name:"1-61",
+            description:"Сифон за мивка гофриран без преливник  1½\""
+        },
+        {
+            image:"sifoni/DSC_0222_.png",
+            name:"1-62",
+            description:"Сифон за мивка  гофриран с преливник 1½\""
+        },
+        {
+            image:"sifoni/DSC_0228_.png",
+            name:"1-64",
+            description:"Сифон за мивка гофриран без преливник и с извод за пералня 1½\""
+        },
+        {
+            image:"sifoni/DSC_0229_.png",
+            name:"1-65",
+            description:"Сифон за мивка гофриран с преливник и с извод за пералня 1½\""
+        },
+        {
+            image:"sifoni/DSC_0233_.png",
+            name:"1-66",
+            description:"Гофрирана тръба ф40 мм./L=640 мм."
+        },
+        {
+            image:"sifoni/DSC_0234_.png",
+            name:"1-67",
+            description:"Комплект-гофрирана тръба ф40 мм./L=640 мм. с гайка ф40 мм. и уплътнение ф40 мм."
+        },
+        {
+            image:"sifoni/DSC_0235_.png",
+            name:"1-68",
+            description:"Гофрирана тръба ф40 мм./L=640 мм. за тоалетно казанче(на Санитапласт АД с.Драгоево)"
+        },
+        {
+            image:"sifoni/DSC_0244_.png",
+            name:"1-71",
+            description:"Гофрирана тръба ф40 мм./L=640 мм. за тоалетно казанче(на Никипласт-М ООД гр.Севлиево)"
+        },
+        {
+            image:"sifoni/DSC_0255_.png",
+            name:"1-75",
+            description:"Сифон за мивка гофриран без преливник и с извод за пералня и миялна машина ф90 мм."
+        },
+        {
+            image:"sifoni/DSC_0268_.png",
+            name:"1-30B1",
+            description:"Сифон с преливник и гофрирана изходяща тръба L=640 мм. за мивка 1½\""
+        },
+        {
+            image:"sifoni/DSC_0284_.png",
+            name:"1-35B1",
+            description:"Сифон без преливник с гофрирана изходяща тръба ф40 мм./L=640 мм. за мивка ф90 мм."
+        },
+        {
+            image:"sifoni/DSC_0285_.png",
+            name:"1-36B1",
+            description:"Сифон с преливник и гофрирана изходяща тръба ф40 мм./L=640 мм. за мивка ф90 мм."
+        },
+        {
+            image:"sifoni/DSC_0269_.png",
+            name:"1-48B1",
+            description:"Сифон без преливник с гофрирана изходяща тръба L=640 мм. за мивка 1½\""
+        },
+        {
+            image:"sifoni/DSC_0271_.png",
+            name:"1-61B1",
+            description:"Сифон за мивка гофриран 1½\" /без маншон ф40 мм./50 мм./"
+        },
+        {
+            image:"sifoni/DSC_0273_.png",
+            name:"1-68B1",
+            description:"Комбинирана тръба ф40 мм./L=870 мм. за тоалетно казанче /гофрирана и твърда/(за Санитапласт АД с.Драгоево)"
+        }
+    ]
+}
+let ventilaciq = {
+    ventilaciq: [
+        {
+            image:"sifoni/DSC_0117_.png",
+            name:"1-22",
+            description:"Вентилационна решетка ф130 мм."
+        },
+        {
+            image:"sifoni/DSC_0121_.png",
+            name:"1-23",
+            description:"Вентилационна решетка ф110 мм."
+        },
+        {
+            image:"sifoni/DSC_0242_.png",
+            name:"1-70",
+            description:"Универсален редуктор ф80-ф150 мм."
+        },
+        {
+            image:"sifoni/DSC_0299_.png",
+            name:"1-80",
+            description:"Клапа за аспиратор ф120 мм."
+        }
+    ]
+}
+let manshoni = {
+    manshoni: [
+        {
+            image:"sifoni/DSC_0171_.png",
+            name:"1-40",
+            description:"Маншон ф32/60 мм."
+        },
+        {
+            image:"sifoni/DSC_0173_.png",
+            name:"1-41",
+            description:"Маншон ф32/50 мм."
+        },
+        {
+            image:"sifoni/DSC_0175_.png",
+            name:"1-42",
+            description:"Маншон ф40/50 мм."
+        },
+        {
+            image:"sifoni/DSC_0202_.png",
+            name:"1-54",
+            description:"Маншон ф40/50 мм."
+        },
+        {
+            image:"sifoni/DSC_0226_.png",
+            name:"1-63",
+            description:"Mаншон ф40/60 мм."
+        },
+        {
+            image:"sifoni/DSC_0238_.png",
+            name:"1-69",
+            description:"Маншон ф25/50 мм."
+        },
+        {
+            image:"sifoni/DSC_0252_.png",
+            name:"1-74",
+            description:"Маншон ф50/60 мм."
+        }
+    ]
+}
+let vikChasti = {
+    vikChasti: [
+        {
+            image:"sifoni/DSC_0163_.png",
+            name:"1-37",
+            description:"Коляно холендър ф50 мм."
+        },
+        {
+            image:"sifoni/DSC_0165_.png",
+            name:"1-38",
+            description:"Коляно холендър ф110 мм."
+        },
+        {
+            image:"sifoni/DSC_0168_.png",
+            name:"1-39",
+            description:"Гайка за тоалетно казанче /редукция/"
+        },
+        {
+            image:"sifoni/DSC_0178_.png",
+            name:"1-43",
+            description:"Нипел ф40 мм."
+        },
+        {
+            image:"sifoni/DSC_0181_.png",
+            name:"1-46",
+            description:"Уплътнение ф50 мм."
+        },
+        {
+            image:"sifoni/DSC_0182_.png",
+            name:"1-47",
+            description:"Уплътнение ф110 мм."
+        },
+        {
+            image:"sifoni/DSC_0186_.png",
+            name:"1-49",
+            description:"Регулируем маншон ф40/60 мм."
+        },
+        {
+            image:"sifoni/DSC_0193_.png",
+            name:"1-52",
+            description:"Тапа за мивка"
+        },
+        {
+            image:"sifoni/DSC_0258_.png",
+            name:"1-76",
+            description:"Купа за гипс"
+        }
+    ]
+}       
+let kraka = {
+    kraka: [
+        {
+            image:"sifoni/DSC_0366_.png",
+            name:"2-11",
+            description:"Крак ОЗ М12"
+        },
+        {
+            image:"sifoni/DSC_0301_.png",
+            name:"2-17",
+            description:"Крак пластмасов 40х40 мм."
+        },
+        {
+            image:"sifoni/DSC_0304_.png",
+            name:"2-18",
+            description:"Комбиниран крак L115/140 (пластмаса,болт M12х35)"
+        },
+        {
+            image:"sifoni/DSC_0307_.png",
+            name:"2-20",
+            description:"Крак пластмасов за стелаж"
+        },
+        {
+            image:"sifoni/DSC_0312_.png",
+            name:"2-23",
+            description:"Тапа пластмасова ф28 мм."
+        },
+        {
+            image:"sifoni/DSC_0347_.png",
+            name:"2-27",
+            description:"Ел. входове ф13мм., ф16мм., ф21мм., ф29мм. "
+        },
+        {
+            image:"sifoni/DSC_0328_.png",
+            name:"2-30",
+            description:"Ръкохватка пластмасова"
+        },
+        {
+            image:"sifoni/DSC_0333_.png",
+            name:"2-31",
+            description:"Пластмасова правоъгълна пета"
+        },
+        {
+            image:"sifoni/DSC_0313_.png",
+            name:"2-32",
+            description:"Ъгъл пластмасов"
+        },
+        {
+            image:"sifoni/DSC_0363_.png",
+            name:"2-35",
+            description:"Крак пластмасов L125/200 мм."
+        },
+        {
+            image:"sifoni/DSC_0315_.png",
+            name:"2-36",
+            description:"Крак пластмасов L45/65 мм."
+        },
+        {
+            image:"sifoni/DSC_0316_.png",
+            name:"2-37",
+            description:"Крак пластмасов 35х35 мм."
+        },
+        {
+            image:"sifoni/DSC_0319_.png",
+            name:"2-38",
+            description:"Крак пластмасов 30х30 мм."
+        },
+        {
+            image:"sifoni/DSC_0320_.png",
+            name:"2-39",
+            description:"Тапа пластмасова 30х30 мм."
+        },
+        {
+            image:"sifoni/DSC_0321_.png",
+            name:"2-44",
+            description:"Тапа пластмасова ф52 мм."
+        },
+        {
+            image:"sifoni/DSC_0335_.png",
+            name:"2-48",
+            description:"Комбиниран крак-пета /пластмаса, болт М10х40, гайка М10/"
+        },
+        {
+            image:"sifoni/DSC_0337_.png",
+            name:"2-48B1",
+            description:"Комбиниран крак-пета /пластмаса, болт М10х60, гайка М10/"
+        },
+        {
+            image:"sifoni/DSC_0326_.png",
+            name:"2-54",
+            description:"Пети пластмасови ф20 мм. и ф26,5 мм."
+        },
+        {
+            image:"sifoni/DSC_0357_.png",
+            name:"2-56",
+            description:"Крак пластмасов L95х145 мм."
+        },
+        {
+            image:"sifoni/DSC_0352_.png",
+            name:"2-61",
+            description:"Връх пластмасов 55х55 мм."
+        },
+        {
+            image:"sifoni/DSC_0346_.png",
+            name:"2-62",
+            description:"Връх пластмасов 10х10 мм. и 15х15 мм."
+        },
+        {
+            image:"sifoni/DSC_0359_.png",
+            name:"2-70",
+            description:"Крак-пета и тапа (с болт М10х40 мм.)"
+        },
+        {
+            image:"sifoni/DSC_0325_.png",
+            name:"2-71",
+            description:"Уплътнение пластмасово за водомер 3/4\""
+        },
+        {
+            image:"sifoni/DSC_0353_.png",
+            name:"2-72",
+            description:"Тапа пластмасова за стелаж"
+        },
+        {
+            image:"sifoni/DSC_0338_.png",
+            name:"2-75",
+            description:"Крак ПМ 35х35 (с болт М10х40 мм.)"
+        },
+        {
+            image:"sifoni/DSC_0342.png",
+            name:"2-75B1",
+            description:"Крак ПМ 35х35 (с болт М10х60 мм.)"
+        }
+    ]
+}
