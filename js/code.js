eac68a36e368b125feef8e05544e11a2521ada2c
Georgi <georgi_yosifov99@abv.bg>
2021-01-19 18:36:14 +0200

    Trying to refactor styles.css

--- js/code.js
+++ js/code.js
@@ -1,8 +1,8 @@
-$( document ).ready(function() {
+$(document).ready(function() {
     let isPopUp = false;
 
     // Function to change the nav-bar on scroll
-    $(window).scroll(function(){
+    $(window).scroll(function() {
         ($(window).scrollTop() >= 100) ? (
             $('.fixed-nav-bar').addClass('scrolled'),
             $('.the-bass').addClass('scrolled'),
@@ -15,9 +15,9 @@ $( document ).ready(function() {
             $('.fixed-nav-bar .logo').removeClass('scrolled')
         );
     });
-    
+
     // Drop Down Function
-    $('#menuButton').on('change', function(){
+    $('#menuButton').on('change', function() {
         ($('#menuButton').is(':checked')) ? (
             $('.the-bass').addClass('dropped')
         ) : (
@@ -31,54 +31,56 @@ $( document ).ready(function() {
             if (!isPopUp) {
                 $(this).find('.fa-expand-arrows-alt').addClass('expandBtn').removeClass('noneExpandBtn');
             }
-        }, 
+        },
         function() {
             $(this).find('.fa-expand-arrows-alt').addClass('noneExpandBtn').removeClass('expandBtn');
         }
     );
 
     // Pop up image
-    $('.image-main-img').click(function(e){   
+    $('.image-main-img').click(function(e) {
         if (!isPopUp) {
             e.stopPropagation();
             let currParent = $(e.target).parent();
             currParent.addClass('popUp').removeClass('image-main');
             $(e.target).removeClass("image-main-img");
             currParent.children('.fa-expand-arrows-alt').addClass('noneExpandBtn').removeClass('expandBtn');
-            currParent.children('i').addClass('closeBtn').removeClass('noneCloseBtn'); 
+            currParent.children('i').addClass('closeBtn').removeClass('noneCloseBtn');
             isPopUp = true;
         }
-    }); 
+    });
+
     //Expand image
-    $('.fa-expand-arrows-alt').click(function(e){
+    $('.fa-expand-arrows-alt').click(function(e) {
         if (!isPopUp) {
             e.stopPropagation();
             let currParent = $(e.target).parent();
             currParent.addClass('popUp').removeClass('image-main');
             currParent.children('img').removeClass("image-main-img");
             currParent.children('.fa-expand-arrows-alt').addClass('noneExpandBtn').removeClass('expandBtn');
-            currParent.children('.fa-times').addClass('closeBtn').removeClass('noneCloseBtn'); 
+            currParent.children('.fa-times').addClass('closeBtn').removeClass('noneCloseBtn');
             isPopUp = true;
-        } 
+        }
     });
 
     //Close pop up image
-    $("body").click(function(e){ 
+    $("body").click(function(e) {
         if (isPopUp && !$(e.target).is('.popUp, .popUp *')) {
             $('.popUp').children('img').addClass("image-main-img");
-            $('.popUp').children('.fa-times').addClass('noneCloseBtn').removeClass('closeBtn'); 
+            $('.popUp').children('.fa-times').addClass('noneCloseBtn').removeClass('closeBtn');
             $('.popUp').children('.fa-expand-arrows-alt').addClass('expandBtn').removeClass('noneExpandBtn');
             $('.popUp').addClass('image-main').removeClass('popUp');
             isPopUp = false;
-        } 
-       
+        }
+
     });
+
     //Close pop up image
-    $('.fa-times').click(function(e){
+    $('.fa-times').click(function(e) {
         $(e.target).parent().addClass('image-main').removeClass('popUp');
         $(e.target).parent().children('img').addClass("image-main-img");
         $(e.target).addClass('noneCloseBtn').removeClass('closeBtn');
-        $(e.target).parent().children('.fa-expand-arrows-alt').addClass('expandBtn').removeClass('noneExpandBtn'); 
-        isPopUp = false;  
+        $(e.target).parent().children('.fa-expand-arrows-alt').addClass('expandBtn').removeClass('noneExpandBtn');
+        isPopUp = false;
     });
 });
--- js/templates.js
+++ js/templates.js
@@ -1,162 +1,163 @@
 $(document).ready(function() {
-  (async function(){
-    hideAll();
-    await renderSifoni114();
-    $('#sifoni114').show();
-    await renderSifoni112();
-    await renderDvoiniSifoni112();
-    await renderPodoviSifoni();
-    await renderGofriraniTrubi();
-    await renderVentilaciq();
-    await renderManshoni();
-    await renderVikChasti();
-    await renderKraka();
-  })();
-  function hideAll() {
-    $('#sifoni114').hide();
-    $('#sifoni112').hide();
-    $('#dvoiniSifoni112').hide();
-    $('#podoviSifoni').hide();
-    $('#gofriraniTrubi').hide();
-    $('#ventilaciq').hide();
-    $('#manshoni').hide();
-    $('#vikChasti').hide();
-    $('#kraka').hide();
-  }
-  async function renderSifoni114() {
-      let template = $('#sifoni114Scr').html();
-      Mustache.parse(template);  
-      await $.get('product-template.mustache', function (partialTemplate) { 
-          let rendered = Mustache.render(template, sifoni114, {
-            sifon114: partialTemplate
-          });
-          $('#sifoni114').html(rendered);
-      }, 'text');
-  }
-  async function renderSifoni112() {
-      let template = $('#sifoni112Scr').html();
-      Mustache.parse(template);  
-      await $.get('product-template.mustache',function (partialTemplate) {  
-          let rendered = Mustache.render(template, sifoni112, {
-            sifon112: partialTemplate
-          });
-          $('#sifoni112').html(rendered);
-      }, 'text');
-  }
-  async function renderDvoiniSifoni112() {
-    let template = $('#dvoiniSifoni112Scr').html();
-    Mustache.parse(template);  
-    await $.get('product-template.mustache',function (partialTemplate) {  
-        let rendered = Mustache.render(template, dvoiniSifoni112, {
-          dvoenSifon112: partialTemplate
-        });
-        $('#dvoiniSifoni112').html(rendered);
-    }, 'text');
-  }
-  async function renderPodoviSifoni() {
-    let template = $('#podoviSifoniScr').html();
-    Mustache.parse(template);  
-    await $.get('product-template.mustache',function (partialTemplate) {  
-        let rendered = Mustache.render(template, podoviSifoni, {
-          podovSifon: partialTemplate
-        });    
-        $('#podoviSifoni').html(rendered);
-    }, 'text');
-  }
-  async function renderGofriraniTrubi() {
-    let template = $('#gofriraniTrubiScr').html();
-    Mustache.parse(template);  
-    await $.get('product-template.mustache',function (partialTemplate) {  
-        let rendered = Mustache.render(template, gofriraniTrubi, {
-          gofriranaTruba: partialTemplate
-        });    
-        $('#gofriraniTrubi').html(rendered);
-    }, 'text');
-  }
-  async function renderVentilaciq() {
-    let template = $('#ventilaciqScr').html();
-    Mustache.parse(template);  
-    await $.get('product-template.mustache',function (partialTemplate) {  
-        let rendered = Mustache.render(template, ventilaciq, {
-          ventilator: partialTemplate
-        });    
-        $('#ventilaciq').html(rendered);
-    }, 'text');
-  }
-  async function renderManshoni() {
-    let template = $('#manshoniScr').html();
-    Mustache.parse(template);  
-    await $.get('product-template.mustache',function (partialTemplate) {  
-        let rendered = Mustache.render(template, manshoni, {
-          manshon: partialTemplate
-        });    
-        $('#manshoni').html(rendered);
-    }, 'text');
-  }
-  async function renderVikChasti() {
-    let template = $('#vikChastiScr').html();
-    Mustache.parse(template);  
-    await $.get('product-template.mustache',function (partialTemplate) {  
-        let rendered = Mustache.render(template, vikChasti, {
-          vikChast: partialTemplate
-        });    
-        $('#vikChasti').html(rendered);
-    }, 'text');
-  }
-  async function renderKraka() {
-    let template = $('#krakaScr').html();
-    Mustache.parse(template);  
-    await $.get('product-template.mustache',function (partialTemplate) {  
-        let rendered = Mustache.render(template, kraka, {
-          krak: partialTemplate
-        });    
-        $('#kraka').html(rendered);
-    }, 'text');
-  }
+    (async function() {
+        hideAll();
+        await renderSifoni114();
+        $('#sifoni114').show();
+        await renderSifoni112();
+        await renderDvoiniSifoni112();
+        await renderPodoviSifoni();
+        await renderGofriraniTrubi();
+        await renderVentilaciq();
+        await renderManshoni();
+        await renderVikChasti();
+        await renderKraka();
+    })();
 
-  $('#sifoni114Btn').click(function(e){
-    e.preventDefault();
-    hideAll();
-    $('#sifoni114').show();
-  });
-  $('#sifoni112Btn').click(function(e){
-    e.preventDefault();
-    hideAll();
-    $('#sifoni112').show();
-  });
-  $('#dvoiniSifoni112Btn').click(function(e){
-    e.preventDefault();
-    hideAll();
-    $('#dvoiniSifoni112').show();
-  });
-  $('#podoviSifoniBtn').click(function(e){
-    e.preventDefault();
-    hideAll();
-    $('#podoviSifoni').show();
-  });
-  $('#gofriraniTrubiBtn').click(function(e){
-    e.preventDefault();
-    hideAll();
-    $('#gofriraniTrubi').show();
-  });
-  $('#ventilaciqBtn').click(function(e){
-    e.preventDefault();
-    hideAll();
-    $('#ventilaciq').show();
-  });
-  $('#manshoniBtn').click(function(e){
-    e.preventDefault();
-    hideAll();
-    $('#manshoni').show();
-  });
-  $('#vikChastiBtn').click(function(e){
-    e.preventDefault();
-    hideAll();
-    $('#vikChasti').show();
-  });
-  $('#krakaBtn').click(function(e){
-    e.preventDefault();
-    hideAll();
-    $('#kraka').show();
-  });
-})
+    function hideAll() {
+        $('#sifoni114').hide();
+        $('#sifoni112').hide();
+        $('#dvoiniSifoni112').hide();
+        $('#podoviSifoni').hide();
+        $('#gofriraniTrubi').hide();
+        $('#ventilaciq').hide();
+        $('#manshoni').hide();
+        $('#vikChasti').hide();
+        $('#kraka').hide();
+    }
+    async function renderSifoni114() {
+        let template = $('#sifoni114Scr').html();
+        Mustache.parse(template);
+        await $.get('product-template.mustache', function(partialTemplate) {
+            let rendered = Mustache.render(template, sifoni114, {
+                sifon114: partialTemplate
+            });
+            $('#sifoni114').html(rendered);
+        }, 'text');
+    }
+    async function renderSifoni112() {
+        let template = $('#sifoni112Scr').html();
+        Mustache.parse(template);
+        await $.get('product-template.mustache', function(partialTemplate) {
+            let rendered = Mustache.render(template, sifoni112, {
+                sifon112: partialTemplate
+            });
+            $('#sifoni112').html(rendered);
+        }, 'text');
+    }
+    async function renderDvoiniSifoni112() {
+        let template = $('#dvoiniSifoni112Scr').html();
+        Mustache.parse(template);
+        await $.get('product-template.mustache', function(partialTemplate) {
+            let rendered = Mustache.render(template, dvoiniSifoni112, {
+                dvoenSifon112: partialTemplate
+            });
+            $('#dvoiniSifoni112').html(rendered);
+        }, 'text');
+    }
+    async function renderPodoviSifoni() {
+        let template = $('#podoviSifoniScr').html();
+        Mustache.parse(template);
+        await $.get('product-template.mustache', function(partialTemplate) {
+            let rendered = Mustache.render(template, podoviSifoni, {
+                podovSifon: partialTemplate
+            });
+            $('#podoviSifoni').html(rendered);
+        }, 'text');
+    }
+    async function renderGofriraniTrubi() {
+        let template = $('#gofriraniTrubiScr').html();
+        Mustache.parse(template);
+        await $.get('product-template.mustache', function(partialTemplate) {
+            let rendered = Mustache.render(template, gofriraniTrubi, {
+                gofriranaTruba: partialTemplate
+            });
+            $('#gofriraniTrubi').html(rendered);
+        }, 'text');
+    }
+    async function renderVentilaciq() {
+        let template = $('#ventilaciqScr').html();
+        Mustache.parse(template);
+        await $.get('product-template.mustache', function(partialTemplate) {
+            let rendered = Mustache.render(template, ventilaciq, {
+                ventilator: partialTemplate
+            });
+            $('#ventilaciq').html(rendered);
+        }, 'text');
+    }
+    async function renderManshoni() {
+        let template = $('#manshoniScr').html();
+        Mustache.parse(template);
+        await $.get('product-template.mustache', function(partialTemplate) {
+            let rendered = Mustache.render(template, manshoni, {
+                manshon: partialTemplate
+            });
+            $('#manshoni').html(rendered);
+        }, 'text');
+    }
+    async function renderVikChasti() {
+        let template = $('#vikChastiScr').html();
+        Mustache.parse(template);
+        await $.get('product-template.mustache', function(partialTemplate) {
+            let rendered = Mustache.render(template, vikChasti, {
+                vikChast: partialTemplate
+            });
+            $('#vikChasti').html(rendered);
+        }, 'text');
+    }
+    async function renderKraka() {
+        let template = $('#krakaScr').html();
+        Mustache.parse(template);
+        await $.get('product-template.mustache', function(partialTemplate) {
+            let rendered = Mustache.render(template, kraka, {
+                krak: partialTemplate
+            });
+            $('#kraka').html(rendered);
+        }, 'text');
+    }
+
+    $('#sifoni114Btn').click(function(e) {
+        e.preventDefault();
+        hideAll();
+        $('#sifoni114').show();
+    });
+    $('#sifoni112Btn').click(function(e) {
+        e.preventDefault();
+        hideAll();
+        $('#sifoni112').show();
+    });
+    $('#dvoiniSifoni112Btn').click(function(e) {
+        e.preventDefault();
+        hideAll();
+        $('#dvoiniSifoni112').show();
+    });
+    $('#podoviSifoniBtn').click(function(e) {
+        e.preventDefault();
+        hideAll();
+        $('#podoviSifoni').show();
+    });
+    $('#gofriraniTrubiBtn').click(function(e) {
+        e.preventDefault();
+        hideAll();
+        $('#gofriraniTrubi').show();
+    });
+    $('#ventilaciqBtn').click(function(e) {
+        e.preventDefault();
+        hideAll();
+        $('#ventilaciq').show();
+    });
+    $('#manshoniBtn').click(function(e) {
+        e.preventDefault();
+        hideAll();
+        $('#manshoni').show();
+    });
+    $('#vikChastiBtn').click(function(e) {
+        e.preventDefault();
+        hideAll();
+        $('#vikChasti').show();
+    });
+    $('#krakaBtn').click(function(e) {
+        e.preventDefault();
+        hideAll();
+        $('#kraka').show();
+    });
+});
